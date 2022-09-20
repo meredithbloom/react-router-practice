b8f9a3505ce8c81758e529e949aa2b7e456c6ddb
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-20 14:08:37 -0400

    added nested comments route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,18 @@
 import { Fragment } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, Route } from "react-router-dom"
+
+import Comments from '../components/comments/Comments'
 
 const QuoteDetail = () => {
     const params = useParams()
 
     return (
         <Fragment>
-        <h1>Quote Detail Page</h1>
+            <h1>Quote Detail Page</h1>
             <p>{params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments/>
+            </Route>    
         </Fragment>
     )
 
