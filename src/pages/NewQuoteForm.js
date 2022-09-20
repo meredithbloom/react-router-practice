297dfd904de537d62c93689a5b83eba1ff0680b0
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-20 17:53:51 -0400

    implemented http requests to google firebase

--- src/pages/NewQuoteForm.js
+++ src/pages/NewQuoteForm.js
@@ -1,13 +1,22 @@
 import QuoteForm from '../components/quotes/QuoteForm'
 import { useHistory } from 'react-router-dom' 
 import useHttp from '../hooks/use-http'
+import { addQuote } from '../lib/api'
+import { useEffect } from 'react'
 
 const NewQuoteForm = () => {
-
+    const {sendRequest, status} = useHttp(addQuote)
     const history = useHistory()
 
+    useEffect(() => {
+        if (status === 'completed') { 
+            history.push('/quotes')
+        }
+    }, [status, history])
+
+
     const addQuoteHandler = (quoteData) => {
-        console.log(quoteData)
+        sendRequest(quoteData)
         history.push('/quotes')
 
     }
@@ -17,7 +26,7 @@ const NewQuoteForm = () => {
 
 
     return (
-            <QuoteForm onAddQuote={addQuoteHandler} />
+        <QuoteForm isLoading={status==='pending'} onAddQuote={addQuoteHandler} />
         )
 
 }
