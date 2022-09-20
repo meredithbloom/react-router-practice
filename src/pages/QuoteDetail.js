8522108baaf5cdb645e1c59fcdcda313d49539b3
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-20 13:58:44 -0400

    added routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,14 @@
+import { Fragment } from "react"
+import { useParams } from "react-router-dom"
+
 const QuoteDetail = () => {
+    const params = useParams()
+
     return (
+        <Fragment>
         <h1>Quote Detail Page</h1>
+            <p>{params.quoteId}</p>
+        </Fragment>
     )
 
 }
